611f82e136ba6ce1971a685f1e98ebe9936de26c
Jaden Eckel <jadeneckel@gmail.com>
2023-02-03 13:03:34 -0600

    initial commit

---
+++ src/comp/EndingModal.js
@@ -0,0 +1,42 @@
+import React from "react";
+import data from "../data";
+import EndingList from "./EndingList";
+
+function EndingModal({ handleClose, endings }) {
+    const endingTotal = 3; //total number of endings
+    const endingList = []; //array to populate which displays list of endings
+
+    //use for loop to populate array
+    for (let i = 1; i < endingTotal + 1; i++) {
+        let showEnding;
+
+        if (endings.includes(i)) {
+            //if youve unlocked this ending
+            showEnding = <EndingList ending={i} data={data} key={i} />; //show
+        } else {
+            showEnding = <li>???</li>;
+        }
+
+        endingList.push(showEnding);
+    }
+    console.log(endingList);
+    //const endingList = endings.map((ending, index) => <EndingList ending={ending} data={data} key={index} />);
+    return (
+        <div className="modalBg" onClick={handleClose}>
+            <div className="modalContainer">
+                <div className="modalContent">
+                    <h1>Endings Reached</h1>
+                    <h2>
+                        {endings.length} / {endingTotal}
+                    </h2>
+                    <ol>{endingList}</ol>
+                </div>
+                <div className="modalFooter">
+                    <h2>Click anywhere to close</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default EndingModal;
---
+++ src/data.js
@@ -0,0 +1,35 @@
+const data = [
+    {
+        id: 0,
+        title: "The Bad Beginning",
+        text:`
+        An old man stands before you and unfolds a map.
+        "Here is the map. Where do you wish to go?"`, //text to display
+        choice: [
+            //choices
+            { id: 0, text: "New York City", goTo: 1 },
+            { id: 1, text: "Boston", goTo: 2 },
+            { id: 2, text: "Santa Fe", goTo: 3 },
+        ],
+    },
+    {
+        id: 1,
+        title: "That's The Life For Me!",
+        text: "You've selected New York City. There is a giant peach stuck through the empire state building.",
+        choice: [],
+    },
+    {
+        id: 2,
+        title: "Boston Translation",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh sem, varius id euismod eget, imperdiet in orci. Quisque auctor nisl ut euismod dapibus. Quisque scelerisque nibh eget est eleifend, a lacinia lorem convallis. Nam pulvinar dui vulputate orci iaculis, vitae consequat ex pellentesque. Morbi condimentum nibh turpis, eget tempor magna sagittis eu. Sed dui quam, dapibus non diam in, imperdiet aliquet lacus. Vestibulum posuere eget elit vitae ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer volutpat sagittis velit, eu venenatis tortor ullamcorper vitae. Morbi finibus ligula urna, ornare vehicula lorem interdum nec. Nunc euismod placerat nibh, quis pretium ante aliquam in. Quisque eu imperdiet erat, quis tincidunt leo. Duis risus urna, venenatis non pharetra nec, venenatis id ligula. Etiam at malesuada ante.",
+        choice: [],
+    },
+    {
+        id: 3,
+        title: "Santa Fail",
+        text: "Santa Claus is not here, neither is Tim Allen",
+        choice: [],
+    },
+];
+
+export default data;
